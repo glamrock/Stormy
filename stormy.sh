1ca2da08a1fa4930f0118fd0cd2e627b6b1b2fad
Griffin Boyce <griffin@cryptolab.net>
2014-09-20 16:19:55 -0400

    ghost dependencies

--- stormy.sh
+++ stormy.sh
@@ -29,29 +29,9 @@ function root {
 }
 
 
-
+#----- VARIOUS ITEMS -----#
 version=$(lsb_release -cs)
 dist=$(lsb_release -is)
-
-
-#----- DISABLE POPULARITY -----#
-
-function popcon {
-
-# Long live the king
-# Note: in Ubuntu, while it is a dep of ubuntu metapackages, removing both might
-# not destroy the system. It is also toggled off by default: PARTICIPATE="no"
-# http://ubuntuforums.org/showthread.php?t=1654103 gives me pause.
-
-    if [ $(dpkg-query -l | grep popularity-contest | wc -c) -ne 0 ];
-    then     
-        if [[ `lsb_release -is` == "Debian" ]] 
-          apt-get purge popularity-contest #not a dependency for Debian
-        elif [[ `lsb_release -is` == "Ubuntu" ]]
-          sed -i '/PARTICIPATE/c\PARTICIPATE="no"' ./etc/popularity-contest.conf
-          chmod -x /etc/cron.daily/popularity-contest #I need more info here
-    fi
-}
 
 
 #----- ADD DEVELOPER KEYS -----#
@@ -61,10 +41,7 @@ function keyfob {
     
     apt-key adv --keyserver keys.mayfirst.org --recv-keys 4A90646C0BAED9D456AB3111E5B81856D0220E4B 35CD74C24A9B15A19E1A81A194373AA94B7C3223 8C4CD511095E982EB0EFBFA21E8BF34923291265 AD1AB35C674DF572FBCE8B0A6BC758CBC11F6276 0D24B36AA9A2A651787876451202821CBE2CD9C1 25FC1614B8F87B52FF2F99B962AF4031C82E0039 261C5FBE77285F88FB0C343266C8C2D7C5AA446D C963C21D63564E2B10BB335B29846B3C683686CC 68278CC5DD2D1E85C4E45AD90445B7AB9ABBEEC6 A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 0291ECCBE42B22068E685545627DEE286B4D6475 02959AA7190AB9E9027E07363B9D093F31B0974B C2E34CFC13C62BD92C7579B56B8AAEB1F1F5C9B5 8738A680B84B3031A630F2DB416F061063FEE659 B35BF85BF19489D04E28C33C21194EBB165733EA F65CE37F04BA5B360AE6EE17C218525819F78451 B1172656DFF983C3042BC699EB5A896A28988BF5 879BDA5BF6B27B6127450A2503CF4A0AB3C79A63 #tor build keys
 
-    apt-key adv --keyserver keys.mayfirst.org --recv-keys 
-
 addsource
-
 }
 
 
@@ -78,14 +55,11 @@ function addsource {
 
 # Detect if Ubuntu
     if [[ `lsb_release -is` == "Ubuntu" ]]
+    echo "$dist $version"
 
 # Detect if Debian
 
     elif [[ `lsb_release -is` == "Debian" ]]
-
-
-
-
 
 # Detect Wat
     else
@@ -101,8 +75,6 @@ function addsource {
 
     wizard #fly off to the wizard function
 }
-
-
 
 # Halper function
 
@@ -124,13 +96,78 @@ function halp {
 	exit;
 }
 
+#----- DISABLE POPULARITY -----#
 
+function popcon {
 
+# Long live the king
+# Note: in Ubuntu, while it is a dep of ubuntu metapackages, removing both might
+# not destroy the system. It is also toggled off by default: PARTICIPATE="no"
+# http://ubuntuforums.org/showthread.php?t=1654103 gives me pause.
+
+    if [ $(dpkg-query -l | grep popularity-contest | wc -c) -ne 0 ];
+    then     
+        if [[ `lsb_release -is` == "Debian" ]] 
+          apt-get purge popularity-contest #not a dependency for Debian
+        elif [[ `lsb_release -is` == "Ubuntu" ]]
+          sed -i '/PARTICIPATE/c\PARTICIPATE="no"' ./etc/popularity-contest.conf
+          chmod -x /etc/cron.daily/popularity-contest #I need more info here
+    fi
+
+cleanup
+}
+
+#----- Install Ghost and related dependencies -----#
+elif [ $INPUT -eq 2 ]; then
+    echo 'Installing dependencies...'
+    apt-get build-dep python-defaults -y -qq
+    apt-get update -y -qq
+    apt-get install python python-dev python-software-properties -y -qq
+
+# NODE
+    apt-get install g++ make nodejs -y -qq
+    apt-get update -y -qq
+    apt-get install npm -y -qq
+    npm install forever -g
+    
+# Double-check for broken deps before finishing up
+    echo 'Checking integrity...'
+    apt-get check -y -qq
+
+# Debian users are less nervous than Ubuntu users, but still.
+    echo 'Dependencies installed!'
+
+#kick over to cleanup
+    cleanup
+
+#----- Cleanup -----#
+# elif [ "$INPUT" -eq 4 ]; then
+
+    clear
+    echo ''
+    echo 'Finishing up!'
+    echo ''
+
+    # Check for broken packages
+    echo 'Checking software integrity'
+    apt-get -f install -y -qq
+
+    # Remove leftover files
+    echo 'Removing leftover packages...'
+    apt-get autoremove -y -qq
+    echo 'Cleaning up temporary cache...'
+    apt-get clean -y -qq
+    echo 'Done.'
+    sleep 5
+
+    clear
+
+log #go to the logout function, just make sure it appears above this entry
 
 
 #----- Logout Dialogue -----#
 
-function logout {
+function log {
     echo 'Please reboot if possible. Your hidden service will start automatically.'
     read -p "(O)kay! / (I) can't yet.    " REPLY 
 
@@ -156,6 +193,15 @@ function end {
         clear && exit
     fi
 
+}
+
+# Return
+# elif [ $INPUT -eq 9 ]; then
+#    clear && wizard
+
+elif [ "$INPUT" -eq 9 ]; then
+    clear && end
+fi
 }
 
 root #start at the beginning
