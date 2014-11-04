ea9b89ec7d7d3f1f8b7526ba70fbfa970bee4c06
Griffin Boyce <griffin@cryptolab.net>
2014-11-04 13:50:20 -0500

    grepfix

--- one-click-blog.sh
+++ one-click-blog.sh
@@ -268,37 +268,33 @@ function server {
 # this includes sending all errors (except 300/301) back to the front page
 # with a code of 200 OK
 
-  bash -c 'cat << EOF > /etc/nginx/sites-available/ghost
-server {  
-    listen 80;
-    server_name localhost;
+#   bash -c 'cat << EOF > /etc/nginx/sites-available/ghost
+# server {  
+#     listen 80;
+#     server_name localhost;
 
-    location / {
-            proxy_pass http://127.0.0.1:2368;
-            proxy_set_header Host $host;
-            proxy_buffering off;
-    }
+#     location / {
+#             proxy_pass http://127.0.0.1:2368;
+#             proxy_set_header Host $host;
+#             proxy_buffering off;
+#     }
 
-    error_page 500 501 502 503 504 505 506 507 508 509 510 =200 /;
-    error_page 302 303 304 305 306 307 =200 /;
-    error_page 400 401 402 403 404 405 406 407 408 409 410 =200 /;
-    error_page 411 412 413 414 415 416 417 418 419 420 421 =200 /;
-    error_page 422 423 424 425 426 =200 /;
+#     error_page 500 501 502 503 504 505 506 507 508 509 510 =200 /;
+#     error_page 302 303 304 305 306 307 =200 /;
+#     error_page 400 401 402 403 404 405 406 407 408 409 410 =200 /;
+#     error_page 411 412 413 414 415 416 417 418 419 420 421 =200 /;
+#     error_page 422 423 424 425 426 =200 /;
 
-    location ^~ /error/ {
-        internal;
-        root /var/www/ghost;
-    }
-}
-EOF'
+#     location ^~ /error/ {
+#         internal;
+#         root /var/www/ghost;
+#     }
+# }
+# EOF'
 
-ln -s /etc/nginx/sites-available/ghost /etc/nginx/sites-enabled/ghost  
-service nginx restart 
+# ln -s /etc/nginx/sites-available/ghost /etc/nginx/sites-enabled/ghost  
+# service nginx restart 
 
-
-
-spooky
-}
 
 
 spooky #let's bring it all together now
@@ -331,7 +327,7 @@ function popcon {
 # not destroy the system. It is also toggled off by default: PARTICIPATE="no"
 # http://ubuntuforums.org/showthread.php?t=1654103 gives me pause.
 
-    if [ $(dpkg-query -l | grep popularity-contest | wc -c) -ne 0 ];
+    if [ $(dpkg-query -l | grep -c popularity-contest) -ne 0 ];
     then     
         if [[ $dist == "Debian" ]]; then
           apt-get purge popularity-contest #not a dependency for Debian
